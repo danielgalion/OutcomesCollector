3027aca17f087f391b289ec5b54da1d03f1ae90a
Daniel Galion <daniel.galion94@gmail.com>
2023-03-04 16:50:32 +0100

    Get picture from camera
    Check permissions. Add them to manifest. Implement Preview.
    
    Use PreviewView and CameraX.
    Simple implementation that has code the most in onCreate functions.
    
    Not handled denied permission.

--- app/src/main/java/com/github/danielgalion/outcomescollector/MainActivity.kt
+++ app/src/main/java/com/github/danielgalion/outcomescollector/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import com.github.danielgalion.outcomescollector.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        requestPermissions(arrayOf(android.Manifest.permission.CAMERA),1)
 
         binding.previewBtn.setOnClickListener {
 
--- app/src/main/java/com/github/danielgalion/outcomescollector/PreviewActivity.kt
+++ app/src/main/java/com/github/danielgalion/outcomescollector/PreviewActivity.kt
@@ -1,11 +1,46 @@
 package com.github.danielgalion.outcomescollector
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.github.danielgalion.outcomescollector.databinding.ActivityPreviewBinding
+import com.google.common.util.concurrent.ListenableFuture
 
 class PreviewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPreviewBinding
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_preview)
+        binding = ActivityPreviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        Log.i("preview", "oncreate further")
+        cameraProviderFuture.addListener({
+            Log.i("preview", "runnable")
+            bindPreview(cameraProviderFuture.get())
+        }, ContextCompat.getMainExecutor(this))
+
+
+    }
+
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview = Preview.Builder().build()
+
+        val cameraSelector =
+            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+        preview.setSurfaceProvider(binding.preview.surfaceProvider)
+
+        val camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
     }
 }
